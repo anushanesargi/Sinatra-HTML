94cb85c61a96ffcd3ad352a25edbd4151e44c34f
Anusha Nesargi <anushanesargi@gmail.com>
2022-03-08 15:24:46 +0000

    Sinatra views

--- app.rb
+++ app.rb
@@ -2,20 +2,26 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 
-get '/' do
-    "hello! ANusha"
-end
+# get '/' do
+#     "hello! ANusha"
+# end
 
-get '/secret' do
-    "hello! You"
-end
+# get '/secret' do
+#     "hello! You"
+# end
 
-get '/firstsecret' do
-    "hello! You Beautiful"
-end
+# get '/firstsecret' do
+#     "hello! You Beautiful"
+# end
+
+# get '/cat' do
+#     <h1>My name is <%= ["Amigo", "Almond", "Cashew", "Misty"].sample%></h1>
+#     "<div style='border: 3px dashed red'>
+#         <img src='https://i.imgur.com/jFaSxym.png/'>
+#     </div>"
+# end
 
 get '/cat' do
-    "<div style='border: 3px dashed red'>
-        <img src='https://i.imgur.com/jFaSxym.png/'>
-    </div>"
+    @name = ["Amigo", "Almond", "Cashew", "Misty"].sample
+    erb(:index)
 end
